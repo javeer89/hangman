c007ecad20616780d2bbc0af5b54a1177b487e5e
Javeer Miah <javeermiah@gmail.com>
2023-11-05 00:45:10 +0000

    minor change to m4

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,10 +1,3 @@
 #%%
-from milestone_3 import make_guess, check_guess, guess_word, word_generator
 
-word = word_generator()
-word_list = list(word)
-make_guess()
 
-print(word)
-print(word_generator())
-print(word_list)
